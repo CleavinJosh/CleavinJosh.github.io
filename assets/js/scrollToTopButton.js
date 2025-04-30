0f38d051a872f283ab628e10b5dbf2e6ba822a2e
CleavinJosh <oriascleavin@gmail.com>
2025-04-30 15:52:56 +0800

    Updated text to be more convinient and added some smooth scrolling and scrolltotop button

---
+++ assets/js/scrollToTopButton.js
@@ -0,0 +1,17 @@
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+// Show the button when the user scrolls down 200px
+window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+scrollToTopBtn.onclick = function() {
+    // Scroll to the top of the document smoothly
+    window.scrollTo({
+        top: 0,
+    });
+}
