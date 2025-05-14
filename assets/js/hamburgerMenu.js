1308b6bd1ade20b04baa15837a79aae2ac4c9105
CleavinJosh <oriascleavin@gmail.com>
2025-05-14 13:46:13 +0800

    Improved hamburger menu, added skip to main content link, and gave functionality to the site logo

--- assets/js/hamburgerMenu.js
+++ assets/js/hamburgerMenu.js
@@ -1,15 +1,36 @@
 const hamburger = document.getElementById('hamburger');
 const closeButton = document.getElementById('close');
 const navMenu = document.getElementById('nav-menu');
+const menuItems = document.querySelectorAll('.nav-item');
+
+let isMenuOpen = false;
 
 hamburger.addEventListener('click', () => {
   navMenu.classList.add('show');
   hamburger.classList.add('hide');
   closeButton.classList.remove('hide');
+  isMenuOpen = true;
 });
 
 closeButton.addEventListener('click', () => {
+  closeMenu();
+});
+
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    closeMenu();
+  });
+});
+
+document.addEventListener('click', (event) => {
+  if (isMenuOpen && !navMenu.contains(event.target) && !hamburger.contains(event.target)) {
+    closeMenu();
+  }
+});
+
+function closeMenu() {
   navMenu.classList.remove('show');
   closeButton.classList.add('hide');
   hamburger.classList.remove('hide');
-});
+  isMenuOpen = false;
+}
---
+++ assets/js/siteLogo.js
@@ -0,0 +1,3 @@
+document.getElementById('siteLogo').addEventListener('click', function() {
+  window.location.href = '/';
+});
