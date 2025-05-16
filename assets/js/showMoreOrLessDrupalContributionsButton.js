38f99b6ef733b048564e32fb4373467cf9df800f
CleavinJosh <oriascleavin@gmail.com>
2025-05-16 11:25:32 +0800

    Updated show more and show less button functionality

--- assets/js/showMoreOrLessDrupalContributionsButton.js
+++ assets/js/showMoreOrLessDrupalContributionsButton.js
@@ -3,15 +3,26 @@ const showLessButton = document.getElementById('show-less-contributions');
 const drupalContributions = document.querySelectorAll('.drupal-contribution');
 
 showMoreButton.addEventListener('click', () => {
+  // Make all contributions visible
   drupalContributions.forEach(contribution => {
-    contribution.classList.remove('drupal-contribution-hidden');
+    contribution.classList.remove('hidden');
   });
-  showMoreButton.classList.add('drupal-contribution-hidden');
-  showLessButton.classList.remove('drupal-contribution-hidden');
+
+  showMoreButton.classList.add('hidden');
+  showLessButton.classList.remove('hidden');
 });
 
 showLessButton.addEventListener('click', () => {
+  // Hide all contributions
   drupalContributions.forEach(contribution => {
-      contribution.classList.add('drupal-contribution-hidden');
+    contribution.classList.add('hidden');
+  });
+
+  showLessButton.classList.add('hidden');
+  showMoreButton.classList.remove('hidden');
+
+  // Scroll to the "Show More" button
+  showMoreButton.scrollIntoView({
+    block: 'center',    // Align the button to the center of the screen
   });
 });
